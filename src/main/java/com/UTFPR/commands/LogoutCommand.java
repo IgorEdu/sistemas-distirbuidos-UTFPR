0255575b7199d68098248e254bbb425ee549463a
Igor <igoreduardo20101@gmail.com>
2024-11-14 16:05:55 -0300

    fix: corrige encerramento prematuro do socket

--- src/main/java/com/UTFPR/commands/LogoutCommand.java
+++ src/main/java/com/UTFPR/commands/LogoutCommand.java
@@ -2,16 +2,12 @@ package com.UTFPR.commands;
 
 import com.UTFPR.domain.dto.LogoutDTO;
 import com.UTFPR.domain.dto.ResponseDTO;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.UTFPR.service.ResponseFormatter;
 import com.UTFPR.service.ResponseService;
 import com.UTFPR.service.UserService;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.Socket;
 
 public class LogoutCommand implements Command {
     private LogoutDTO logoutDTO;
@@ -37,13 +33,6 @@ public class LogoutCommand implements Command {
         String response = responseFormatter.formatResponse(responseDTO);
         System.out.println("Server (" + clientAddress + "): " + response);
 
-//                    System.out.println("Client (" + clientSocket.getInetAddress() + "): " + inputLine);
-//                    System.out.println("Cliente desconectado: " + clientSocket.getInetAddress());
-//                    System.out.println("Server (" + clientSocket.getInetAddress() + "): " + response);
-
         out.println(response);
-        out.close();
-//        in.close();
-//        clientSocket.close();
     }
 }
