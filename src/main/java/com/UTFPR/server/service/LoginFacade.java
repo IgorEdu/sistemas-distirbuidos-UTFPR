f3c7b0b8c1260946328bb9552703f1e9ae686904
Igor <igoreduardo20101@gmail.com>
2024-11-28 17:29:33 -0300

    refactor: insere padrão facade para login

---
+++ src/main/java/com/UTFPR/server/service/LoginFacade.java
@@ -0,0 +1,75 @@
+package com.UTFPR.server.service;
+
+import com.UTFPR.domain.dto.LoginDTO;
+import com.UTFPR.domain.dto.ResponseDTO;
+import jakarta.persistence.PersistenceException;
+import org.hibernate.JDBCException;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class LoginFacade {
+    private final UserService userService;
+    private final ResponseService responseService;
+    private final ResponseFormatter responseFormatter;
+
+    public LoginFacade(UserService userService, ResponseService responseService, ResponseFormatter responseFormatter) {
+        this.userService = userService;
+        this.responseService = responseService;
+        this.responseFormatter = responseFormatter;
+    }
+
+    public String handleLogin(LoginDTO loginDTO, String clientAddress) throws IOException {
+        String formattedResponse;
+        ResponseDTO responseDTO;
+
+
+        try {
+            if (!userService.isValidRa(loginDTO) || !userService.isValidPassword(loginDTO)) {
+                responseDTO = responseService.createErrorResponse(
+                        loginDTO.getOperacao(),
+                        "Os campos recebidos nao sao validos."
+
+                );
+                formattedResponse = responseFormatter.formatResponse(responseDTO);
+                System.out.println("Server (" + clientAddress + "): " + formattedResponse);
+                return formattedResponse;
+            }
+
+            if (!userService.isExistentUser(loginDTO)) {
+                responseDTO = responseService.createErrorResponse(
+                        loginDTO.getOperacao(),
+                        "Credenciais incorretas."
+                );
+
+                formattedResponse = responseFormatter.formatResponse(responseDTO);
+                System.out.println("Server (" + clientAddress + "): " + formattedResponse);
+                return formattedResponse;
+            }
+
+
+            String token = userService.generateRaToken(loginDTO.getRa());
+            responseDTO = responseService.createSuccessResponseWithToken(token);
+            formattedResponse = responseFormatter.formatResponse(responseDTO);
+            System.out.println("Server (" + clientAddress + "): " + formattedResponse);
+        } catch (PersistenceException e) {
+            responseDTO = responseService.createErrorResponse(
+                    loginDTO.getOperacao(),
+                    "O servidor nao conseguiu conectar com o banco de dados"
+            );
+            formattedResponse = responseFormatter.formatResponse(responseDTO);
+            System.out.println("Server (" + clientAddress + "): " + formattedResponse);
+            e.printStackTrace();
+        }
+        catch (Exception e) {
+            responseDTO = responseService.createErrorResponse(
+                    loginDTO.getOperacao(),
+                    "Erro interno no servidor."
+            );
+            formattedResponse = responseFormatter.formatResponse(responseDTO);
+            System.out.println("Server (" + clientAddress + "): " + formattedResponse);
+            e.printStackTrace();
+        }
+        return formattedResponse;
+    }
+}
