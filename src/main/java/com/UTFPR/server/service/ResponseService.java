068a7dd7efc06071e8093735a1608633cf3f4bca
Igor <igoreduardo20101@gmail.com>
2024-12-09 18:53:12 -0300

    fix: corrige envios de mensagens quando deve ir operação na resposta

--- src/main/java/com/UTFPR/server/service/ResponseService.java
+++ src/main/java/com/UTFPR/server/service/ResponseService.java
@@ -1,27 +1,31 @@
 package com.UTFPR.server.service;
 
 import com.UTFPR.domain.dto.ResponseDTO;
+import com.UTFPR.domain.entities.User;
 
 public class ResponseService {
 
 
-    public ResponseDTO createSuccessResponse() {
+    public ResponseDTO createSuccessResponse(String operacao) {
         ResponseDTO response = new ResponseDTO();
         response.setStatus(200);
+//        response.setOperacao(operacao);
         return response;
     }
 
-    public ResponseDTO createSuccessResponseWithToken(String token) {
+    public ResponseDTO createSuccessResponseWithToken(String operacao, String token) {
         ResponseDTO response = new ResponseDTO();
         response.setStatus(200);
         response.setToken(token);
+//        response.setOperacao(operacao);
         return response;
     }
 
-    public ResponseDTO createSuccessResponseWithMessage(String message) {
+    public ResponseDTO createSuccessResponseWithMessage(String operacao, String message) {
         ResponseDTO response = new ResponseDTO();
         response.setStatus(200);
         response.setMensagem(message);
+        response.setOperacao(operacao);
         return response;
     }
 
@@ -31,6 +35,21 @@ public class ResponseService {
         response.setStatus(401);
         response.setOperacao(operacao);
         response.setMensagem(message);
+        return response;
+    }
+
+    public ResponseDTO returnSuccessResponseUserInformations(String operacao, User user) {
+        String message = String.format("""
+                {
+                    "nome": "%s",
+                    "ra": "%s"
+                }
+                """, user.getNome(), user.getRa());
+
+        ResponseDTO response = new ResponseDTO();
+        response.setStatus(200);
+        response.setMensagem(message);
+//        response.setOperacao(operacao);
         return response;
     }
 }
